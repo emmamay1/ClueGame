c29a76ee9d4aa3ebdc1773ca2e5e4e9da99c29bf
Emma May <emmamay@mines.edu>
2018-02-27 12:15:33 -0700

    Set up classes and began test design

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,5 +1,71 @@
 package experiment;
 
+import java.util.Map;
+import java.util.Set;
+
 public class IntBoard {
+	private Map<BoardCell, Set<BoardCell>> adjMtx;
+	private Set<BoardCell> visited;
+	private Set<BoardCell> targets;
+	private BoardCell[][] grid;
+	
+
+	public IntBoard() {
+		super();
+		initializeGrid();
+	}
+
+	public void initializeGrid() {
+		grid = new BoardCell[4][4];
+	}
+	
+	public IntBoard(Map<BoardCell, Set<BoardCell>> adjMtx) {
+		this.adjMtx = adjMtx;
+	}
+	
+	/**
+	 * Calculates the adjacency list
+	 *  for each grid cell and stores the results as a Map in adjMtx
+	 */
+	public void calcAdjacencies(){
+		
+	}
+	
+	/**
+	 * Returns the adjacency list for one cell
+	 * @param cell
+	 * @return
+	 */
+	public Set<BoardCell> getAdjList(BoardCell cell){
+		return null;
+	}
+	
+	/**
+	 * Calculates the targets that are pathLength distance from the startCell. 
+	 * The list of targets will be stored as a Set in an instance variable.
+	 * @param startCell
+	 * @param pathLength
+	 */
+	public void calcTargets(BoardCell startCell, int pathLength){
+		
+	}
+	
+	/**
+	 * Returns the list of targets as a set
+	 * @return
+	 */
+	public Set<BoardCell> getTargets(){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	public BoardCell getCell(int i, int j) {
+		return null;
+	}
 
 }
--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -1,5 +1,115 @@
 package tests;
+import experiment.BoardCell;
+import experiment.IntBoard;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class IntBoardTests {
+	private IntBoard board;
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Before
+    public void beforeAll() {
+       board = new IntBoard();  // constructor should call calcAdjacencies() so you can test them
+    }
+	
+	  /*
+		 * Test adjacencies for top left corner
+		 */
+		@Test
+		public void testAdjacency00()
+		{
+			BoardCell cell = board.getCell(0,0);
+			Set<BoardCell> testList = board.getAdjList(cell);
+			assertTrue(testList.contains(board.getCell(1, 0)));
+			assertTrue(testList.contains(board.getCell(0, 1)));
+			assertEquals(2, testList.size());
+		}
+		
+		  /*
+			 * Test adjacencies for bottom right corner
+			 */
+			@Test
+			public void testAdjacency33()
+			{
+				BoardCell cell = board.getCell(3,3);
+				Set<BoardCell> testList = board.getAdjList(cell);
+				assertTrue(testList.contains(board.getCell(3, 2)));
+				assertTrue(testList.contains(board.getCell(2, 3)));
+				assertEquals(2, testList.size());
+			}
+			
+			  /*
+			 * Test adjacencies for right edge
+			 */
+			@Test
+			public void testAdjacency13()
+			{
+				BoardCell cell = board.getCell(1,3);
+				Set<BoardCell> testList = board.getAdjList(cell);
+				assertTrue(testList.contains(board.getCell(0, 3)));
+				assertTrue(testList.contains(board.getCell(1, 2)));
+				assertTrue(testList.contains(board.getCell(2, 3)));
+				assertEquals(3, testList.size());
+			}
+			
+			  /*
+			 * Test adjacencies for top left edge
+			 */
+			@Test
+			public void testAdjacency30()
+			{
+				BoardCell cell = board.getCell(3,0);
+				Set<BoardCell> testList = board.getAdjList(cell);
+				assertTrue(testList.contains(board.getCell(2, 0)));
+				assertTrue(testList.contains(board.getCell(3, 1)));
+				assertEquals(2, testList.size());
+			}
+			
+			  /*
+			 * Test adjacencies for (1, 1)
+			 */
+			@Test
+			public void testAdjacency11()
+			{
+				BoardCell cell = board.getCell(1,1);
+				Set<BoardCell> testList = board.getAdjList(cell);
+				assertTrue(testList.contains(board.getCell(1, 0)));
+				assertTrue(testList.contains(board.getCell(0, 1)));
+				assertTrue(testList.contains(board.getCell(1, 2)));
+				assertTrue(testList.contains(board.getCell(2, 1)));
+				assertEquals(4, testList.size());
+			}
+			
+			  /*
+			 * Test adjacencies for (2,2)
+			 */
+			@Test
+			public void testAdjacency22()
+			{
+				BoardCell cell = board.getCell(2,2);
+				Set<BoardCell> testList = board.getAdjList(cell);
+				assertTrue(testList.contains(board.getCell(1, 2)));
+				assertTrue(testList.contains(board.getCell(2, 1)));
+				assertTrue(testList.contains(board.getCell(3, 2)));
+				assertTrue(testList.contains(board.getCell(2, 3)));
+				assertEquals(4, testList.size());
+			}
+			
+			@Test
+			public void testTarget(){
+				
+			}
+
 
 }
