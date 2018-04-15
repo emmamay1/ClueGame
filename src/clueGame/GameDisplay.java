0a081fd53fef40ab1300c6329bd226e8825f5f9e
dshowman3413 <dshowman@mymail.mines.edu>
2018-04-15 16:08:56 -0600

    Fixed merge issue in GameDisplay

--- src/clueGame/GameDisplay.java
+++ src/clueGame/GameDisplay.java
@@ -5,31 +5,18 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 
 public class GameDisplay extends JFrame{
-<<<<<<< HEAD
-	
-
-=======
 	private static Board board;
->>>>>>> 08edca6a50dde1528b6945db1ba9612f353f9ffe
 	public GameDisplay() {
 		board = board.getInstance();
 		board.setConfigFiles("ClueBoardLayout.csv", "Rooms.txt", "People.txt", "Weapons.txt");
 		board.initialize();
 		setLayout(new BorderLayout());
 		GUIControlPanel controlPanel = new GUIControlPanel();
-<<<<<<< HEAD
-		//GameBoardPanel boardPanel = new GameBoardPanel();
+		GameBoardPanel boardPanel = new GameBoardPanel(board);
 		YourCardsPanel cardPanel = new YourCardsPanel();
 		add(controlPanel, BorderLayout.SOUTH);
-		//add(boardPanel, BorderLayout.CENTER);
-		add(cardPanel, BorderLayout.EAST);	
-=======
-		GameBoardPanel boardPanel = new GameBoardPanel(board);
-		//YourCardsPanel cardPanel = new YourCardsPanel();
-		add(controlPanel, BorderLayout.SOUTH);
 		add(boardPanel, BorderLayout.CENTER);
-		//add(cardPanel, BorderLayout.EAST);	
->>>>>>> 08edca6a50dde1528b6945db1ba9612f353f9ffe
+		add(cardPanel, BorderLayout.EAST);	
 	}
 	
 	public static void main(String[] arg0) {
